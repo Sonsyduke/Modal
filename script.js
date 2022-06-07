bc5ddaffd7f96a664546f7a8af795d531e6e94f5
Sonsyduke <douglaslombard29@gmail.com>
2022-06-07 10:00:50 +0200

    adding event listeners to open and close the modal

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
-'use strict';
+"use strict";
+
+// Selecting all the classes we'll be using
+const modal = document.querySelector(".modal");
+const overlay = document.querySelector(".overlay");
+const btnCloseModal = document.querySelector(".close-modal");
+const btnsOpenModal = document.querySelectorAll(".show-modal");
+
+// Function to open modal
+function openModal() {
+  modal.classList.remove("hidden");
+  overlay.classList.remove("hidden");
+}
+
+// Function to close modal
+function closeModal() {
+  modal.classList.add("hidden");
+  overlay.classList.add("hidden");
+}
+
+// Looping through the buttons and adding an event listener
+for (let i = 0; i < btnsOpenModal.length; i++) {
+  btnsOpenModal[i].addEventListener("click", openModal);
+}
+
+// adding event listener to close the modal
+btnCloseModal.addEventListener("click", closeModal);
+overlay.addEventListener("click", closeModal);
